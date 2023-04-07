d6c2ac607237be4d903f523b8b38a540b7973b04
Semavi Ulusoy <doc.divxm@gmail.com>
2023-04-07 16:31:19 +0300

    sm8350-common: AICPfy
     * LiveDisplay is disabled.
     * enable OplusParts common overlays
        -- disable vibration strenght
        -- disable usb to fast charge
     * TouchGestures
    
    Change-Id: I16549897574679aaf70b85956974db1190488cba

--- common.mk
+++ common.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
+# PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
 
 # Media
@@ -289,7 +289,7 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aicp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
@@ -398,6 +398,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
+    TouchGestures \
     vendor.lineage.touch@1.0-service.oplus
 
 # Update engine
